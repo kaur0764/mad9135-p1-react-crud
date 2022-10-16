88fd80e4ae7c147aff83d7627ebabb4de5fefccb
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-10-16 18:59:28 -0400

    Add the AppHeader component

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AppHeader() {
+  return (
+    <header className="App-header">
+      <p>To Watch List</p>
+    </header>
+  );
+}
+
+export default AppHeader;
